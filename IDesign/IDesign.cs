d82ac739d2952f1d2434b4db055256cf655e8bed
HungryMuttis <hungrymuttis@gmail.com>
2024-10-18 18:09:09 +0300

    Updated IDesign

--- IDesign/IDesign.cs
+++ IDesign/IDesign.cs
@@ -3,7 +3,7 @@ namespace Modder
     public interface IDesign
     {
         public delegate void LogEvent(object obj, LogType type);
-        public delegate void StartProgramEvent(Type[] modNames);
+        public delegate void StartProgramEvent(IGameMod[] modNames);
 
         abstract event IDesign.LogEvent? Log; // (Fired by Design) the Designs way of telling the program to log something
         abstract event IDesign.StartProgramEvent? StartProgram; // (Fired by Design) when fired, all the given mods will be executed (the program will be started)
